2d992c4f483d7f81f275470c29c452232eff3557
Muddavugga <dennisalvey423@gmail.com>
2017-09-15 10:10:20 -0700

    issues with modules

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+//  the html routes are to help direct the user. 
+// Whenever they click on a link, the router will understand, based on this info
+// what page to deliver to them.
+var path = require('path');
+module.exports = function (app) {
+	
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '../survey.html'));
+	});
+
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname + '../public/home.html'));
+	});
+
+	// app.use(function(req, res){
+	// 	res.sendFile(path.join(__dirname + '../public/home.html'));
+	// 	console.log("you should be reading this")
+	// });
+	
+}
